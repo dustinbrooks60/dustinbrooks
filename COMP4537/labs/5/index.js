7ef6e5dc7ff24c2096cfe9aa179b52908f71cb40
dustinbrooks60 <dbrooks49@my.bcit.ca>
2021-02-13 21:21:07 -0800

    updated

--- COMP4537/labs/5/index.js
+++ COMP4537/labs/5/index.js
@@ -6,7 +6,7 @@ function checkData() {
     sendInfo();
   }
   else {
-    window.alert("Your score is not a number, try again.");
+    window.alert("Check your data and try again.");
   }
 }
 
@@ -14,8 +14,8 @@ function sendInfo() {
   const xhttp = new XMLHttpRequest();
   let name = document.getElementById("name").value;
   let score = parseInt(document.getElementById("score").value);
-  xhttp.open("GET", "https://dustin-brooks-60.netlify.app/comp4537/labs/5/writedb/?name=" + name + "&score=" + score , true);
-  xhttp.send(name, score);
+  xhttp.open("GET", "https://rocky-temple-74799.herokuapp.com/?name=" + name + "&score=" + score , true);
+  xhttp.send();
 
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -25,18 +25,3 @@ function sendInfo() {
 }
 
 
-
-
-function retrieveInfo() {
-  const xhttp = new XMLHttpRequest();
-  let name = document.getElementById("name").value;
-  let score = parseInt(document.getElementById("score").value);
-  xhttp.open("GET", "https://dustin-brooks-60.netlify.app/comp4537/labs/5/writedb/?name=" + name + "&score=" + score , true);
-  xhttp.send();
-
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("data").innerHTML = this.responseText;
-    }
-  };
-}
